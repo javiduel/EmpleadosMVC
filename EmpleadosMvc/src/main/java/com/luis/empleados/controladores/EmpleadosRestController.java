92e96e46df6f5f6417f7e5aa7e296bc77f8dfd3f
OOP <OOP@teide-PC>
2014-07-18 20:41:47 +0200

    creacion de borrar

--- EmpleadosMvc/src/main/java/com/luis/empleados/controladores/EmpleadosRestController.java
+++ EmpleadosMvc/src/main/java/com/luis/empleados/controladores/EmpleadosRestController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,11 +32,21 @@ public class EmpleadosRestController {
 
 @RequestMapping(method=RequestMethod.GET, value="/buscar/{texto}")
    public @ResponseBody List<Empleado> buscar (@PathVariable String texto){
+	if(texto.equals("NoBuscoNada"))
+		texto="";
 	Map<String,Object> params=new HashMap();//una lista clave valor 
 	params.put("texto","%"+texto+"%");//da igual la posicion de la palabra que ponga si buscar a luis sera todo que contenga luis
 	List<Empleado> l=dao.find("empleado.buscador",params);
 	return l;
    }
-	
+@RequestMapping(method=RequestMethod.DELETE)
+    public @ResponseBody String borrar(@RequestBody Empleado empleado){
+
+	 dao.delete(empleado);
+
+	return "borrado";	
+
+}
+
  }
                 
